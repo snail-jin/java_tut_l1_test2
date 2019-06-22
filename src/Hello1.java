9ed2545cb0d2a7c9b1808ce38e8b27bc28b8b5ca
snail-jin <52083685+snail-jin@users.noreply.github.com>
2019-06-21 20:58:49 -0700

    Update Hello1.java

--- src/Hello1.java
+++ src/Hello1.java
@@ -4,7 +4,7 @@ public class Hello1 {
 		System.out.println("My second Java Program");
 		System.out.println("I love Java");
 		System.out.println("Hello!");
-		System.out.println("Github is cool!");
+		System.out.println("Github is STILL VERY cool!");
 	}
 
 }
